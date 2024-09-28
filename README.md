a960bf64d0adb8c384882f7155844275a9b2b807
chatbot-1 <getupmaverick@gmail.com>
2024-09-28 13:01:56 +0530

    Frontend setup

--- README.md
+++
@@ -1,1 +0,0 @@
-# itsa_hack
---
+++ app/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className='text-center text-2xl'>Doctor's appointment app</p>
+    </>
+  )
+}
+
+export default App
